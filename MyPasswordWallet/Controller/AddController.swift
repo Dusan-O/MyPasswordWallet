82d9e4c1cc9799793b9c6f4271f30e3c273e7802
Dusan-O <dusan.orescanin@gmail.com>
2022-08-15 19:17:31 +0200

    UI and IBOutlet + IBActions: done

---
+++ MyPasswordWallet/Controller/AddController.swift
@@ -0,0 +1,38 @@
+//
+//  AddController.swift
+//  MyPasswordWallet
+//
+//  Created by Dusan Orescanin on 15/08/2022.
+//
+
+import UIKit
+
+class AddController: UIViewController {
+    
+    
+    @IBOutlet weak var siteTextField: UITextField!
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var pwdTextField: UITextField!
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+
+}
---
+++ MyPasswordWallet/Controller/DetailController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailController.swift
+//  MyPasswordWallet
+//
+//  Created by Dusan Orescanin on 15/08/2022.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+    
+    @IBOutlet weak var siteLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var showHidePwd: UILabel!
+    @IBOutlet weak var showSwitch: UISwitch!
+    @IBOutlet weak var pwdLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+    }
+    
+
+
+}
